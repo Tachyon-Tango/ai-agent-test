7b872359568fc06b8f200ae8df73323f68f79141
Tachyon Tango <TachyonTango@pm.me>
2025-07-07 23:46:57 -0700

    Added promp argument functionality to the project.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import os
+import sys
 from dotenv import load_dotenv
+from google import genai
+
+# Grab the API Key and pass to Google GenAi
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
-from google import genai
-
 client = genai.Client(api_key=api_key)
+
+# Get and Parse system arguments
+
+num_args = len(sys.argv)
+
+if num_args != 2:
+    os._exit(1)
+
+user_prompt = sys.argv[1]
+
+
+# Actual Generation of the prompt
 
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', 
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    contents=user_prompt
 )
 
 print(response.text)
