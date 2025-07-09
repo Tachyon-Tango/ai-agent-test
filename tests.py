b81ecbf503b38a07994c4193b048cf9838b7885e
Tachyon Tango <TachyonTango@pm.me>
2025-07-08 23:01:52 -0700

    Replaced tests.py with example for easier clarity. Already passed with previous version though.

--- tests.py
+++ tests.py
@@ -1,11 +1,24 @@
-# from subdirectory.filename import function_name
 from functions.get_files_info import get_files_info
 
-print("\n---\n")
-print(get_files_info("calculator", "."))
-print("\n---\n")
-print(get_files_info("calculator", "pkg"))
-print("\n---\n")
-print(get_files_info("calculator", "/bin"))
-print("\n---\n")
-print(get_files_info("calculator", "../"))
+
+def test():
+    result = get_files_info("calculator", ".")
+    print("Result for current directory:")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "/bin")
+    print("Result for '/bin' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
